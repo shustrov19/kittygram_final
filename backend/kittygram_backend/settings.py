b67dc0f5656f4f7791376c1176f48c8ec463bd91
Виктор Шустров <shustrov19@gmail.com>
2023-08-21 01:56:39 +0300

    Deploy final v9

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5433)
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
